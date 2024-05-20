8cba4cba414345ff6f02313183955e79733bad4b
Lindist <four181049@gmail.com>
2024-05-20 15:33:05 +0700

    Update js

--- margin.js
+++ margin.js
@@ -1,10 +1,6 @@
 
 let m = document.querySelector(".margin");
 
-<<<<<<< HEAD
 m.style.marginLeft = "15%";
 m.style.marginRight = "15%";
-=======
-m.style.marginLeft = "10%";
-m.style.marginRight = "10%";
->>>>>>> 98cb16c43b8101f787a706c912333f97453ff513
+
