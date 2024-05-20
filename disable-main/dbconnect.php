3456d153bee47d47399325664dace0e808e94be6
KwwClw <neskung50@gmail.com>
2024-05-20 12:08:56 +0700

    Update dbconnect.php

--- disable-main/dbconnect.php
+++ disable-main/dbconnect.php
@@ -2,8 +2,8 @@
 
 $connect = mysqli_connect("localhost", "root", "", "database_disables") or die("Could not connect to database");
 
-// if ($connect) {
-//     echo "connecting";
-// }
+if ($connect) {
+    echo "connecting";
+}
 
 ?>
